2d138e4b6ee3cf493166dd9163efea6f89f4f3d8
FLance03 <60454430+FLance03@users.noreply.github.com>
2021-08-19 00:35:32 +0800

    Completed connected components grouping with some first testing

--- main.py
+++ main.py
@@ -2,39 +2,19 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-from Steps import SaturationAndOutlier2 as Step2
+from Steps import (SaturationAndOutlier2 as Step2,
+                   ConnectedComponentGrouping5 as Step5)
 from testing import testing
 
-img = cv.imread('./testing/iot.jpg')
-img = Step2.main(img, CELLGROUPSIZE=20)
-
-cv.imshow('Image Step 2', img)
-# Step 3: Binarization
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv.imread('./testing/pics and texts/iotbinarized.jpg', 0)
+labels, textLabels, wordLabels, phraseLabels, nonTextLabels = Step5.main(img)
 
 
-_, otsu = cv.threshold(gray, 0, 255, cv.THRESH_OTSU)
-# Remove the possible lines from the blackboard edges
-img = otsu[15:-15]
-cv.imshow('Otsu', otsu)
-# Step 4: Closing (removing black) then Opening (removing white)
-# For now nothing sah cause image had alot of 1 pixel thick lines
-# plt.subplot(1, 2, 1)
-# plt.imshow(img, 'gray')
-# plt.title('Image')
-#
-# kernel = np.ones((2, 1), np.uint8)
-# img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
-# img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
-# plt.subplot(1, 2, 2)
-# plt.imshow(img, 'gray')
-# plt.title('Close')
-# plt.show()
-
-# Step 5 Component Grouping
-# img = ConnectedComponentGrouping5(img)
-cv.imwrite('iotbinarized.jpg', img)
-cv.imshow('Close', img)
+cv.imshow('Labels', testing.imshow_components(labels))
+cv.imshow('Texts', testing.imshow_components(textLabels))
+cv.imshow('Words', testing.imshow_components(wordLabels))
+cv.imshow('Phrases', testing.imshow_components(phraseLabels))
+cv.imshow('Non Texts', testing.imshow_components(nonTextLabels))
 
 cv.waitKey()
 cv.destroyAllWindows()
