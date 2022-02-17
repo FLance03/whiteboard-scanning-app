c34dbd4c2006ef5476ab52c3270feca76d1555bc
FLance03 <60454430+FLance03@users.noreply.github.com>
2022-02-18 00:25:54 +0800

    Fixed TestAutomation.py by increasing threshold for the lowerbound of colors from 75 to 100

--- main.py
+++ main.py
@@ -38,24 +38,24 @@ for ind, testImage in enumerate(testImages):
     # cv.destroyAllWindows()
     if ind not in []: # change here
         img = Step1.Preprocessing1(img)
-        # cv.imshow(str(ind) + '.png', testing.ResizeWithAspectRatio(img, height=500))
-        # cv.waitKey()
-        # cv.destroyAllWindows()
+        cv.imshow(str(ind) + '.png', testing.ResizeWithAspectRatio(img, height=500))
+        cv.waitKey()
+        cv.destroyAllWindows()
     img = Step2.main(img)
+    # cv.imshow('Original', testing.ResizeWithAspectRatio(img, height=500))
+    # cv.waitKey()
+    # cv.destroyAllWindows()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     _, bw = cv.threshold(gray, 0, 255, cv.THRESH_OTSU)
     kernel = np.ones((2, 2),np.uint8)
-    cv.imwrite(str(ind) + 'p.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
-    cv.imwrite(str(ind) + 'n.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
-    cv.imwrite(str(ind) + 'o.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
     bw = cv.morphologyEx(bw, cv.MORPH_CLOSE, kernel)
     bw = cv.morphologyEx(bw, cv.MORPH_OPEN, kernel)
     bw = cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA)
     bw = bw[15:-15]
+    cv.imwrite(str(ind) + 'p.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
+    cv.imwrite(str(ind) + 'n.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
+    cv.imwrite(str(ind) + 'o.jpg', cv.resize(bw, dsize=(int(bw.shape[1] * height / float(bw.shape[0])), height), interpolation=cv.INTER_AREA))
     # anded.append(np.where(bw == 0, 1, 0))
-    # cv.imshow('Original', testing.ResizeWithAspectRatio(bw, height=500))
-    # cv.waitKey()
-    # cv.destroyAllWindows()
     # Remove the possible lines from the blackboard edges
     # labels, labelsInfo, textLabels, wordLabels, phraseLabels, nonTextLabels
 
